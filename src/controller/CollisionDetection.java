95bf73d6d79e06e0f61177bba646cde5750f262a
floodski <jflo015@aucklanduni.ac.nz>
2018-03-23 13:49:52 +1300

    Added CollisionDetection class stub and basic movement logic

---
+++ src/controller/CollisionDetection.java
@@ -0,0 +1,8 @@
+package controller;
+
+//this class will check if a move is valid
+// i.e there is no obstacle or boundary in the way of the intended move
+
+public class CollisionDetection {
+
+}
--- src/model/Character.java
+++ src/model/Character.java
@@ -13,4 +13,23 @@ public class Character {
 		xPos = xStart;
 		yPos = yStart;
 	}
+	
+	public void moveLeft() {
+		xPos -= 1;
+	}
+	
+	public void moveUp() {
+		yPos += 1;
+	}
+	
+	public void moveRight() {
+		//TODO will update position of char if not at some boundary TBC
+		//if(!collisionDetect()){
+			xPos += 1;
+			//}
+	}
+	
+	public void moveDown() {
+		yPos -= 1;
+	}
 }
