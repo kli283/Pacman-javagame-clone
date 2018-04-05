84b1c4a674230bd7e044790328bb15e63e50309a
floodski <jflo015@aucklanduni.ac.nz>
2018-04-05 23:16:57 +1200

    Added a handler to detect when keys are pressed as a test. Currently implemented in the mainapp, may stay there and lose userInput

--- src/controller/MainApp.java
+++ src/controller/MainApp.java
@@ -5,13 +5,16 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import view.UserInput;
 
 public class MainApp extends Application {
 	
@@ -23,11 +26,7 @@ public class MainApp extends Application {
 		this.primaryStage = primaryStage;
 		this.primaryStage.setTitle("Test Drive");
 		
-		initRootLayout();
-	}
-	
-	public void initRootLayout() {
-        try {
+		try {
             // Load root layout from fxml file.
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(MainApp.class.getResource("TestMap.fxml"));
@@ -37,10 +36,32 @@ public class MainApp extends Application {
             Scene scene = new Scene(rootLayout);
             primaryStage.setScene(scene);
             primaryStage.show();
+            
+          //Initialise user input to listen for key presses
+            ArrayList<String> input = new ArrayList<String>();
+            
+    		scene.setOnKeyPressed(
+    				new EventHandler<KeyEvent>()
+    				{	
+    					public void handle(KeyEvent e)
+    					{
+    						String code = e.getCode().toString();
+    						if(!input.contains(code)) {
+    							input.add(code);
+    							System.out.println("Key Pressed");
+    						}
+    					}
+    				});
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
+		
+		
+		
+	}
+	
+	
+	
 
 	public static void main(String[] args) {
 		launch(args);
--- src/view/UserInput.java
+++ src/view/UserInput.java
@@ -1,10 +1,30 @@
 package view;
 
-import java.util.ArrayList;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class UserInput {
+public class UserInput implements KeyListener {
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		System.out.println("Rattata");
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		System.out.println("Over");
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 	
-	ArrayList<String> input = new ArrayList<String>();
+	
 	
 	
 	
