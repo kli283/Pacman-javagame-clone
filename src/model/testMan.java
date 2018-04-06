077b5020813ccfbc497f31232bdbf46943b8c033
floodski <jflo015@auckland.ac.nz>
2018-04-06 13:54:53 +1200

    Added more character fields and sub class

---
+++ src/model/testMan.java
@@ -0,0 +1,15 @@
+package model;
+
+public class testMan extends Character {
+
+	public testMan(double xStart, double yStart, boolean isPlayer) {
+		super(xStart, yStart, isPlayer);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
