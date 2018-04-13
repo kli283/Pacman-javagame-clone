d02d4907335931cd02ffe2aee29398fb5aef8c8d
floodski <jflo015@aucklanduni.ac.nz>
2018-04-13 22:55:08 +1200

    Restructure of UI elements to better reflect MVC design

--- src/view/GameUI.java
+++ src/view/GameUI.java
@@ -1,9 +1,23 @@
 package view;
 
+import java.util.ArrayList;
+
+import javax.xml.stream.events.Characters;
+
 public class GameUI {
+	
+	public GameUI() {
+		
+	}
+	
+	public void draw(ArrayList<Character> actors) {
+		for(Character x:actors) {
+			x.getImageView().relocate(x.getXPos(), x.getYPos());
+		}
+	}
 	
 	public void update() {
 
 	}
-
+	
 }
